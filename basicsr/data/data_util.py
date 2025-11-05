6ee76051765757fd5c0b02c2751aae6aa5716330
hzfrancesca <hzffrancesca@gmail.com>
2025-11-05 22:21:01 +0800

    Refactor paired path generation in data_util.py to use zip for improved readability and efficiency. Update folder argument descriptions for clarity.

--- basicsr/data/data_util.py
+++ basicsr/data/data_util.py
@@ -235,27 +235,21 @@ def paired_paths_from_folder(folders, keys, filename_tmpl):
         f'{input_key} and {gt_key} datasets have different number of images: '
         f'{len(input_paths)}, {len(gt_paths)}.')
     paths = []
-    for idx in range(len(gt_paths)):
-        gt_path = gt_paths[idx]
-        basename, ext = osp.splitext(osp.basename(gt_path))
-        input_path = input_paths[idx]
-        basename_input, ext_input = osp.splitext(osp.basename(input_path))
-        input_name = f'{filename_tmpl.format(basename)}{ext_input}'
-        input_path = osp.join(input_folder, input_name)
-        assert input_name in input_paths, (f'{input_name} is not in '
-                                           f'{input_key}_paths.')
+    for gt_path, input_path in zip(gt_paths, input_paths):
         gt_path = osp.join(gt_folder, gt_path)
+        input_path = osp.join(input_folder, input_path)
         paths.append(
             dict([(f'{input_key}_path', input_path),
                   (f'{gt_key}_path', gt_path)]))
     return paths
+
 
 def paired_DP_paths_from_folder(folders, keys, filename_tmpl):
     """Generate paired paths from folders.
 
     Args:
         folders (list[str]): A list of folder path. The order of list should
-            be [input_folder, gt_folder].
+            be [inputL_folder, inputR_folder, gt_folder].
         keys (list[str]): A list of keys identifying folders. The order should
             be in consistent with folders, e.g., ['lq', 'gt'].
         filename_tmpl (str): Template for each filename. Note that the
